56ef09846bc68a0e0a01c51fc004bd63142bba2a
Rajarshi Das <rd@rajarshi.me>
2022-12-28 21:57:42 +0530

    minor updates.

--- w3apps/website/application/views/homepage.php
+++ w3apps/website/application/views/homepage.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Appengine</title>
-    <link rel="icon" href="images/cafe.png">
-
+    <link rel="icon" href="images/favicon.png">
     <link href="box/bs5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="box/style.css" rel="stylesheet" type="text/css">
     <script src="box/jquery.min.js"></script>
-
 </head>
-<body>
-<div class='container box1'>
 
-    <div class="banner">
-        <p>ci3appengine</p>
-    </div>
-    <div class="story">
-    <p>
-        CodeIgniter3 with multi-app architecture. 
-    </p>
+<body>
+    <div class='container'>
+        <div class="rd-banner">
+            <p>ci3appengine</p>
+        </div>
+        <div class="rd-content">
+            <p>
+                CodeIgniter3 with multi-app architecture.
+            </p>
+        </div>
     </div>
     <script src="box/bs5/js/bootstrap.min.js"></script>
-    </body>
+</body>
+
 </html>
