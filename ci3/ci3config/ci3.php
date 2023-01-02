b98001c0cd5cdfaaddafa25a118a3462a02c6b8b
Rajarshi Das <rd@rajarshi.me>
2023-01-02 23:34:01 +0530

    logging enabled

--- ci3/ci3config/ci3.php
+++ ci3/ci3config/ci3.php
@@ -9,3 +9,4 @@
 define('APPFOLDER', "/home/rd/Apps/ci3appengine");
 define('CI3', APPFOLDER."/ci3/framework");
 define('W3ROOT', APPFOLDER."/w3apps/website/w3root");
+define('W3FILES', APPFOLDER."/w3files");
