a8ea5fb19eb0081514a7b57caae1f37e8ce6ad05
Rajarshi Das <rd@rajarshi.me>
2023-02-01 10:01:45 +0530

    small tweaks

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
+me=$(whoami)
+mygroup=$(id -gn)
 
-# Global
-sudo chown -R rd:rd ..
+# Appfolder User and Group settings
+sudo chown -R ${me}:${mygroup} ..
 sudo find .. \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
 
-# setup script
+# Add execution flag for bash scripts
 chmod +x setup.sh
 chmod +x ci3/bin/*.sh
 
 # Writable folder
 foldername=w3files
 
-sudo chown -R rd:www-data $foldername
+sudo chown -R ${me}:www-data $foldername
 sudo find $foldername \( -type d -exec chmod 770 {} \; \) -o \( -type f -exec chmod 660 {} \; \)
-
-
